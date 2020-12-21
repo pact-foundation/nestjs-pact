555d67a1deeafbfac30cf0b1034a7f917f7b8ba5
Omer Morad <omer.moradd@gmail.com>
2020-12-21 11:33:12 +0200

    Feature/patch/update broken file and readme (#3)
    * Updated Readme file, fix broken file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export * from './services/pact-factory.service';
 export * from './common/pact-module-providers.enum';
 export * from './modules/pact-producer.module';
 export * from './modules/pact-consumer.module';
-export * from 'src/common/provider-factory';
+export * from './common/provider-factory';
 export * from './interfaces/pact-producer-module-options.interface';
 export * from './interfaces/pact-consumer-module-options.interface';
