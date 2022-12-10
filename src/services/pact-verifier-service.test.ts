153e5fbc88eb35140b00631c3977f349a5bff2ca
Lennart Querter <lennart.querter@gmail.com>
2022-12-10 17:51:16 +0100

    fix: revert to normal mocks for verifier test

--- src/services/pact-verifier-service.test.ts
+++ src/services/pact-verifier-service.test.ts
@@ -1,9 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PactModuleProviders } from '../common/pact-module-providers.enum';
 import { PactVerifierService } from './pact-verifier.service';
-import { INestApplication } from '@nestjs/common';
-import { Verifier } from '@pact-foundation/pact';
 import { PactProviderOptions } from '../interfaces/pact-provider-module-options.interface';
+import { INestApplication } from '@nestjs/common';
 
 jest.mock('get-port', () => () => 80);
 
@@ -11,8 +10,15 @@ describe('PactVerifierService', () => {
   let moduleRef: TestingModule;
   let pactVerifierService: PactVerifierService;
 
-  const appMock = jest.createMockFromModule<INestApplication>('');
-  const pactVerifierMock = jest.createMockFromModule<Verifier>('');
+  const appMock = {
+    getUrl: jest.fn().mockResolvedValueOnce('http://127.0.0.1:80'),
+    listen: jest.fn().mockResolvedValueOnce(true),
+    close: jest.fn().mockResolvedValueOnce(true),
+  };
+
+  const pactVerifierMock = {
+    verifyProvider: jest.fn().mockReturnValue(Promise.resolve('some-result')),
+  };
 
   let options: PactProviderOptions = { providerBaseUrl: 'http://127.0.0.1:80' };
 
@@ -35,19 +41,13 @@ describe('PactVerifierService', () => {
   });
 
   beforeEach(() => {
-    jest.resetAllMocks();
-
-    pactVerifierMock.verifyProvider = jest.fn().mockReturnValue(Promise.resolve('some-result'));
-
-    appMock.getUrl = jest.fn().mockResolvedValueOnce('http://127.0.0.1:80');
-    appMock.listen = jest.fn().mockResolvedValueOnce(true);
-    appMock.close = jest.fn().mockResolvedValueOnce(true);
+    jest.clearAllMocks();
   });
 
   describe('when configuring the providerBaseHost', () => {
     beforeEach(async () => {
       options = { ...options, providerBaseUrl: 'http://127.0.0.1:80' };
-      await pactVerifierService.verify(appMock);
+      await pactVerifierService.verify(appMock as unknown as INestApplication);
     });
     test('allows configuring hosts', async () => {
       expect(appMock.listen).toHaveBeenCalledWith(80, '127.0.0.1');
