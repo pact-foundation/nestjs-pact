3c3827a6e9dc9fe24a7b412de009deeca1ac79ff
Yousaf Nabi <yousafn@gmail.com>
2026-09-22 18:42:10 +0100

    Revert "Revert "feat!: support nestjs v12 / vite upgrade""
    This reverts commit 932a3f668f6f953fdb30076f5d2bc1f6799c637a.

--- src/services/pact-verifier-service.test.ts
+++ src/services/pact-verifier-service.test.ts
@@ -1,21 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { vi } from 'vitest';
 
 import { PactProviderOptions } from '../interfaces/pact-provider-module-options.interface';
 import { PactModuleProviders } from '../common/pact-module-providers.enum';
 
 import { PactVerifierService } from './pact-verifier.service';
 
-jest.mock('get-port', () => () => Promise.resolve(1234));
+vi.mock('get-port', () => ({ default: Promise.resolve(1234) }));
 
 describe('PactVerifierService', () => {
   let moduleRef: TestingModule;
   let pactVerifierService: PactVerifierService;
 
   const appMock = {
-    getUrl: jest.fn().mockResolvedValue('http://127.0.0.1:8080'),
-    listen: jest.fn().mockResolvedValueOnce(true),
-    close: jest.fn().mockResolvedValueOnce(true),
+    getUrl: vi.fn().mockResolvedValue('http://127.0.0.1:8080'),
+    listen: vi.fn().mockResolvedValueOnce(true),
+    close: vi.fn().mockResolvedValueOnce(true),
   };
 
   const options: PactProviderOptions = { providerHost: 'http://127.0.0.1:8080' };
@@ -35,7 +36,7 @@ describe('PactVerifierService', () => {
   });
 
   beforeEach(() => {
-    jest.clearAllMocks();
+    vitest.clearAllMocks();
   });
 
   describe('when configuring the providerHost', () => {
